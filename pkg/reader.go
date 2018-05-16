4c0cc711389d301d573b43c7ae94d6f025c1af5b
xokc <xokc@mail.ru>
2018-05-16 23:02:51 +0300

    =

--- pkg/reader.go
+++ pkg/reader.go
@@ -2,37 +2,41 @@ package utils
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	wav "github.com/youpy/go-wav"
 )
 
-// WavReader - wrapper for reading signal samples from wav file
-type WavReader struct {
-	reader wav.Reader
+// SamplesReader - wrapper for reading signal samples from wav file
+type SamplesReader struct {
+	reader     *wav.Reader
 	SampleRate uint32
 	SampleType string
-	Samples map[uint16][]float32
+	Samples    []float32
 }
 
-// NewWavReader - constructor for WavReader
-func NewWavReader(fileName string, []durations int) (*WavReader, error) {
-	file, err := os.Open(FileName)
+// NewSamplesReader - constructor for WavReader
+func NewSamplesReader(fileName string) (*SamplesReader, error) {
+	file, err := os.Open(fileName)
 	defer file.Close()
 	if err != nil {
-		log.Panic("Unable to open file", err)
-		return
+		return nil, fmt.Errorf("Unable to open source file %s: %v", fileName, err)
 	}
-	wr := wav.NewReader(file)
-	format, err := wr.Format()
+	r := wav.NewReader(file)
+	format, err := r.Format()
 	if err != nil {
-		log.Panic("Unable to read wav header", err)
-		return
+		return nil, fmt.Errorf("Unable to read wav header: %v", err)
 	}
-	fmt.Printf("(%d kHz, %d%s)\n", format.SampleRate/1000, format.BitsPerSample, af)
-	fmt.Printf("Try to read %d samples... ", count := int(ms) * int(format.SampleRate) / 1000)
-	samples := reader.NewSamples(&wr, cfg.Data.MaxDuration())
+	if format.NumChannels != 2 {
+		return nil, fmt.Errorf("Unable to process non-iq files")
+	}
+	af, ok := map[uint16]string{wav.AudioFormatPCM: "i", wav.AudioFormatIEEEFloat: "f"}[format.AudioFormat]
+	if !ok {
+		return nil, fmt.Errorf("Unsupported format type %d", format.AudioFormat)
+	}
+
+	wr := SamplesReader{r, format.SampleRate, af, nil}
+	return &wr, nil
 }
 
 type sampleMaker func([]byte, int) (float32, float32)
@@ -58,52 +62,19 @@ func from32s(data []byte, offset int) (r, i float32) {
 	return
 }
 
-// ReadSamples read count pcm samples from wav file by fileName
-func ReadSamples(reader *wav.Reader, count int) ([]float32, error) {
-	file, err := os.Open(cfg.FileName)
-	defer file.Close()
-	if err != nil {
-		return nil, fmt.Errorf("Unable to open file: %v", err)
-	}
-	reader := wav.NewReader(file)
-	format, err := reader.Format()
-	if err != nil {
-		return nil, fmt.Errorf("Unable to read wav header", err)
-	}
-	if format.NumChannels != 2 {
-		return nil, fmt.Errorf("Unable to process non-iq files")
-	}
-	af, ok := map[uint16]string{wav.AudioFormatPCM: "i", wav.AudioFormatIEEEFloat: "f"}[format.AudioFormat]
-	if !ok {
-		return nil, fmt.Errorf("Unsupported format type", format.AudioFormat)
-	}
-	fmt.Printf("(%d kHz, %d%s)\n", format.SampleRate/1000, format.BitsPerSample, af)
-
-	var ms uint16
-	for _, val := range cfg.Data.Duration {
-		if val > ms {
-			ms = val
-		}
-	}
-	if ms == 0 {
-		log.Fatal("Data durations is too small")
-		return
-	}
-
-	count := int(ms) * int(format.SampleRate) / 1000
-	fmt.Printf("Try to read %d samples... ", count)
+// Read read count pcm samples from wav file by fileName
+func (r *SamplesReader) Read(count int) error {
+	format, _ := r.reader.Format()
 	bytes := make([]byte, count*int(format.BlockAlign))
-	n, err := reader.Read(bytes)
+	n, err := r.reader.Read(bytes)
 	if err != nil {
-		log.Panic("Unable to read source file samples", err)
-		return
+		return fmt.Errorf("Unable to read source file samples %v", err)
 	}
 	if n < len(bytes) {
-		log.Fatalf("Source file is too short (%d bytes from %d)", n, len(bytes))
-		return
+		return fmt.Errorf("Source file is too short (%d bytes from %d)", n, len(bytes))
 	}
 
-	samples := make([]float32, count*2)
+	r.Samples = make([]float32, count*2)
 	var fn sampleMaker
 	if format.AudioFormat == wav.AudioFormatIEEEFloat {
 		fn = from16s
@@ -111,9 +82,8 @@ func ReadSamples(reader *wav.Reader, count int) ([]float32, error) {
 		fn = from32s
 	}
 	for i, offset := 0, 0; i < count; i++ {
-		samples[i*2], samples[i*2+1] = fn(bytes, offset)
+		r.Samples[i*2], r.Samples[i*2+1] = fn(bytes, offset)
 		offset += int(format.BlockAlign)
 	}
-	fmt.Println("Ok")
-	return &cfg, nil
+	return nil
 }
